f84f200fccf748056ffe6ebdc630a0e7dd9235f3
chan493 <chandav@hkg118mac04dev.local>
2017-08-09 18:57:04 +0800

    Add Webview

---
+++ testing/testing/Classes/Controllers/ViewControllers/WebPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebPageViewController.h
+//  testing
+//
+//  Created by Chan, Davy on 9/8/2017.
+//  Copyright © 2017 dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebPageViewController : UIWebView
+
+@end
---
+++ testing/testing/Classes/Controllers/ViewControllers/WebPageViewController.m
@@ -0,0 +1,21 @@
+//
+//  WebPageViewController.m
+//  testing
+//
+//  Created by Chan, Davy on 9/8/2017.
+//  Copyright © 2017 dev. All rights reserved.
+//
+
+#import "WebPageViewController.h"
+
+@implementation WebPageViewController
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
